e4f49d21707e9a6351f0d88c984782880792687e
Juliani <julianivn@yahoo.com.br>
2026-02-15 20:16:59 +0000

    SuperTrunfo_mestre_

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,6 +20,21 @@ População: O número de habitantes da cidade. Tipo: int
 Área (em km²): A área da cidade em quilômetros quadrados. Tipo: float
 PIB: O Produto Interno Bruto da cidade. Tipo: float
 Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade. Tipo: int
+
+
+MESTRE:
+-Calcular o Super Poder: Para cada carta, calcule o "Super Poder" somando todos os atributos numéricos 
+(população, área, PIB, número de pontos turísticos, 
+PIB per capita e o inverso da densidade populacional – quanto menor a densidade, maior o "poder"). 
+Armazene o Super Poder como float. Atenção: Preste muita atenção na conversão de tipos ao somar valores de tipos diferentes!
+ 
+-Comparar as Cartas: Compare as duas cartas atributo por atributo (exceto estado, código e nome), 
+incluindo o Super Poder. Lembre-se: para a densidade populacional, a carta com o menor valor vence; 
+para os demais atributos (incluindo Super Poder), a carta com o maior valor vence.
+ 
+-Exibir os Resultados das Comparações: Para cada atributo, imprima na tela qual carta venceu (Carta 1 ou Carta 2), 
+exibindo o resultado da comparação 
+(1 para verdadeiro – Carta 1 vence – e 0 para falso – Carta 2 vence).
 */
 
 #include <stdio.h>
